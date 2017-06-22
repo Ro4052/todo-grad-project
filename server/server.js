2a62ee136cf23953be06973f5f89e47691448f3c
Ronan Graham <rgraham@scottlogic.co.uk>
2017-06-22 11:34:43 +0100

    Replace filter function in getTodo with _.find

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var _ = require("underscore");
 
-module.exports = function(port, middleware, callback) {
+module.exports = function (port, middleware, callback) {
     var app = express();
 
     if (middleware) {
@@ -14,7 +15,7 @@ module.exports = function(port, middleware, callback) {
     var todos = [];
 
     // Create
-    app.post("/api/todo", function(req, res) {
+    app.post("/api/todo", function (req, res) {
         var todo = req.body;
         todo.id = latestId.toString();
         latestId++;
@@ -24,16 +25,16 @@ module.exports = function(port, middleware, callback) {
     });
 
     // Read
-    app.get("/api/todo", function(req, res) {
+    app.get("/api/todo", function (req, res) {
         res.json(todos);
     });
 
     // Delete
-    app.delete("/api/todo/:id", function(req, res) {
+    app.delete("/api/todo/:id", function (req, res) {
         var id = req.params.id;
         var todo = getTodo(id);
         if (todo) {
-            todos = todos.filter(function(otherTodo) {
+            todos = todos.filter(function (otherTodo) {
                 return otherTodo !== todo;
             });
             res.sendStatus(200);
@@ -43,22 +44,26 @@ module.exports = function(port, middleware, callback) {
     });
 
     function getTodo(id) {
-        return todos.filter(function(todo) {
+        console.log(todos)
+        const filteredTodos = _.find(todos, function (todo) {
+            console.log(todo, todo.id)
             return todo.id === id;
-        })[0];
+        })
+        console.log(id, filteredTodos)
+        return filteredTodos[0];
     }
 
     var server = app.listen(port, callback);
 
     // We manually manage the connections to ensure that they're closed when calling close().
     var connections = [];
-    server.on("connection", function(connection) {
+    server.on("connection", function (connection) {
         connections.push(connection);
     });
 
     return {
-        close: function(callback) {
-            connections.forEach(function(connection) {
+        close: function (callback) {
+            connections.forEach(function (connection) {
                 connection.destroy();
             });
             server.close(callback);
